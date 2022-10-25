0df746b63ad6c3b220036fb0c466930231bd106d
Danil <danylo.makhov@nure.ua>
2022-10-25 21:38:26 +0300

    fix local storage

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-alert('Прошу проверить в последний день в сроки не получилось вложиться')
+// alert('Прошу проверить в последний день в сроки не получилось вложиться')
 const body = document.body;
 let Board;
 const cellCount = 4;
@@ -49,12 +49,12 @@ const initRandomArray = (cellCount) => {
       existed[existed.length - 1] = 0;
     }
   }
-  let c = 0
-  for(key in cords) {
-    cords[key].value = existed[c]
-    cords[key].x = cords[key].x * 100
-    cords[key].y = cords[key].y * 100
-    c++
+  let c = 0;
+  for (key in cords) {
+    cords[key].value = existed[c];
+    cords[key].x = cords[key].x * 100;
+    cords[key].y = cords[key].y * 100;
+    c++;
   }
   return existed;
 };
@@ -66,7 +66,7 @@ const defaultHtml = (cellCount, board) => {
   let arrayOfString = [];
   for (let i = 0; i < board.length; i++) {
     arrayOfString.push(
-      `<div class="main-item" id="${board[i]}">${board[i]}</div> `
+      `<div class="main-item" draggable="true" id="${board[i]}">${board[i]}</div> `
     );
   }
   res = arrayOfString.join('');
@@ -76,34 +76,59 @@ const defaultHtml = (cellCount, board) => {
 };
 
 const localStorageGets = (cordsa) => {
-  let generateBoard = []
-  cords = cordsa
-  let arrayOfStrings = []
+  let generateBoard = [];
+  cords = cordsa;
+  let arrayOfStrings = [];
 
-  for(let key in cordsa) {
-    generateBoard.push(cordsa[key].value)
+  for (let key in cordsa) {
+    generateBoard.push(cordsa[key].value);
     arrayOfStrings.push(
       `<div class="main-item"  id="${cordsa[key].value}">${cordsa[key].value}</div> `
     );
   }
-  Board = generateBoard
+  Board = generateBoard;
   return `${`<div class="restart__btn">Restart</div>  <button class="save">save</button><div class="sound__btn">sound</div><div class="_moves">Moves :0</div><div class="_timer">0s</div><div class="main grid-cell-4" >${arrayOfStrings.join(
     ''
   )}</div>`}`;
-}
-if(JSON.parse(localStorage.getItem('cords')) !== null) {
-  body.insertAdjacentHTML('afterbegin', localStorageGets(JSON.parse(localStorage.getItem('cords'))))
-}else {
+};
+if (JSON.parse(localStorage.getItem('cords')) !== null) {
+  body.insertAdjacentHTML(
+    'afterbegin',
+    localStorageGets(JSON.parse(localStorage.getItem('cords')))
+  );
+  Board = JSON.parse(localStorage.getItem('board'))
+  time = JSON.parse(localStorage.getItem('time'))
+  moves = JSON.parse(localStorage.getItem('moves'))
+
+} else {
   body.insertAdjacentHTML('afterbegin', defaultHtml(cellCount, Board));
 }
 document.querySelector('.save').addEventListener('click', () => {
-  if(JSON.parse(localStorage.getItem('cords')) === null) {
-    localStorage.setItem('cords', JSON.stringify(cords))
-  }else {
+  if (JSON.parse(localStorage.getItem('cords')) === null) {
+    localStorage.setItem('cords', JSON.stringify(cords));
+  } else {
     localStorage.removeItem('cords');
-    localStorage.setItem('cords', JSON.stringify(cords))
+    localStorage.setItem('cords', JSON.stringify(cords));
   }
-})
+  if(JSON.parse(localStorage.getItem('board')) === null) {
+    localStorage.setItem('board', JSON.stringify(Board))
+  }else {
+    localStorage.removeItem('board');
+    localStorage.setItem('board', JSON.stringify(Board));
+  }
+  if(JSON.parse(localStorage.getItem('time')) === null) {
+    localStorage.setItem('time', time)
+  }else {
+    localStorage.removeItem('time');
+    localStorage.setItem('time', time);
+  }
+  if(JSON.parse(localStorage.getItem('moves')) === null) {
+    localStorage.setItem('moves', moves)
+  }else {
+    localStorage.removeItem('moves');
+    localStorage.setItem('moves', moves);
+  }
+});
 const collectionItems = document.querySelectorAll('.main-item');
 for (let i = 0; i < collectionItems.length; i++) {
   // collectionItems[i].onmousedown = (e) => {
@@ -117,8 +142,23 @@ for (let i = 0; i < collectionItems.length; i++) {
   collectionItems[i].style.top = cords[i].y + 'px';
 }
 const main = document.querySelector('.main');
+// main.addEventListener('dragstart', (e) => {
+//   e.target.classList.add(`selected`);
+// })
+// main.addEventListener('dragend', (e) => {
+//   e.target.classList.remove(`selected`);
+// })
+// main.addEventListener('dragover', (e) => {
+//   e.preventDefault()
+//   const activeEl = document.querySelector('.selected')
+//   const currentElement = e.target;
+//   const isMoveable = activeEl !== currentElement &&
+//   currentElement.classList.contains(`main-item`);
+//   if (!isMoveable) {
+//     return;
+//   }
+// })
 main.addEventListener('click', (e) => {
-
   let clickedBlock = e.target.innerHTML;
   let need;
   let ChangeBoard = false;
@@ -137,12 +177,16 @@ main.addEventListener('click', (e) => {
                 collectionItems[j].style.top = `${
                   +collectionItems[j].style.top.split('px')[0] + 100
                 }px`;
-                cords[i].y = cords[i].y + 100
+                for (let p = 0; p < cellCount ** 2; p++) {
+                  if (cords[p].value === Board[i]) {
+                    cords[p].y = cords[p].y + 100;
+                  }
+                }
               } else if (+collectionItems[j].innerHTML === Board[i + 4]) {
                 collectionItems[j].style.top = `${
                   +collectionItems[j].style.top.split('px')[0] - 100
                 }px`;
-                cords[j].y = cords[j].y - 100
+                cords[j].y = cords[j].y - 100;
               }
             }
           } else if (Board[i - 4] !== undefined && Board[i - 4] === 0) {
@@ -156,12 +200,17 @@ main.addEventListener('click', (e) => {
                 collectionItems[j].style.top = `${
                   +collectionItems[j].style.top.split('px')[0] - 100
                 }px`;
-                cords[i].y = cords[i].y - 100
+
+                for (let p = 0; p < cellCount ** 2; p++) {
+                  if (cords[p].value === Board[i]) {
+                    cords[p].y = cords[p].y - 100;
+                  }
+                }
               } else if (+collectionItems[j].innerHTML === Board[i - 4]) {
                 collectionItems[j].style.top = `${
                   +collectionItems[j].style.top.split('px')[0] + 100
                 }px`;
-                cords[j].y = cords[j].y + 100
+                cords[j].y = cords[j].y + 100;
               }
             }
           } else if (Board[i + 1] !== undefined && Board[i + 1] === 0) {
@@ -175,12 +224,16 @@ main.addEventListener('click', (e) => {
                 collectionItems[j].style.left = `${
                   +collectionItems[j].style.left.split('px')[0] + 100
                 }px`;
-                cords[i].x = cords[i].x + 100
+                for (let p = 0; p < cellCount ** 2; p++) {
+                  if (cords[p].value === Board[i]) {
+                    cords[p].x = cords[p].x + 100;
+                  }
+                }
               } else if (+collectionItems[j].innerHTML === Board[i + 1]) {
                 collectionItems[j].style.left = `${
                   +collectionItems[j].style.left.split('px')[0] - 100
                 }px`;
-                cords[j].x = cords[j].x - 100
+                cords[j].x = cords[j].x - 100;
               }
             }
           } else if (Board[i - 1] !== undefined && Board[i - 1] === 0) {
@@ -194,12 +247,16 @@ main.addEventListener('click', (e) => {
                 collectionItems[j].style.left = `${
                   +collectionItems[j].style.left.split('px')[0] - 100
                 }px`;
-                cords[i].x = cords[i].x + 100
+                for (let p = 0; p < cellCount ** 2; p++) {
+                  if (cords[p].value === Board[i]) {
+                    cords[p].x = cords[p].x - 100;
+                  }
+                }
               } else if (+collectionItems[j].innerHTML === Board[i - 1]) {
                 collectionItems[j].style.left = `${
                   +collectionItems[j].style.left.split('px')[0] + 100
                 }px`;
-                cords[j].x = cords[j].x + 100
+                cords[j].x = cords[j].x + 100;
               }
             }
           } else {
@@ -231,13 +288,12 @@ main.addEventListener('click', (e) => {
 </div>`
       );
       document.querySelector('.btn').addEventListener('click', () => {
-        if(JSON.parse(localStorage.getItem('cords')) !== null) {
-          localStorage.clear()
-        }else {
-          console.log('local storage is empty')
+        if (JSON.parse(localStorage.getItem('cords')) !== null) {
+          localStorage.removeItem('cords');
+        } else {
+          console.log('local storage is empty');
         }
       });
-      
     }
   }
 });
@@ -256,11 +312,10 @@ document.querySelector('.sound__btn').addEventListener('click', () => {
 });
 
 document.querySelector('.restart__btn').addEventListener('click', () => {
-  if(JSON.parse(localStorage.getItem('cords')) !== null) {
-    localStorage.clear()
-    window.location.reload()
-  }else {
-    console.log('local storage is empty')
-    window.location.reload()
+  if (JSON.parse(localStorage.getItem('cords')) !== null) {
+    localStorage.removeItem('cords');
+    window.location.reload();
+  } else {
+    window.location.reload();
   }
 });
